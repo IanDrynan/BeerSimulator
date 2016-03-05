ebbb3e0d4b75d98255a93a816f85ff6754562977
Daniel Borstelmann <dborstelmann@gmail.com>
2016-03-05 16:50:12 -0600

    dan's old assignment

---
+++ Assign1-DanielBorstelmann/Mushroom-Roll/Assets/Scripts/PlayerController.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var speed : float;
+private var score : int;
+public var scoreText : GUIText;
+public var winText : GUIText;
+
+function Start(){
+	score = 0;
+	SetCountText();
+	winText.text = "";
+}
+
+function FixedUpdate () {
+	var moveHorizontal = Input.GetAxis("Horizontal");
+	var moveVertical = Input.GetAxis("Vertical");
+	
+	var movement = Vector3(moveHorizontal, 0.0f, moveVertical);
+	
+	GetComponent.<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+}
+
+function OnTriggerEnter(other : Collider){
+	if (other.gameObject.tag == 'PickUp'){
+		other.gameObject.SetActive(false);
+		score ++;
+		SetCountText();
+	}
+}
+
+function SetCountText(){
+	scoreText.text = 'Score: ' + score.ToString();
+	if (score == 15)
+		winText.text = "You win!";
+}
---
+++ Assign1-DanielBorstelmann/Mushroom-Roll/Assets/Scripts/Rotator.js
@@ -0,0 +1,4 @@
+﻿#pragma strict
+function Update () {
+	transform.Rotate(Vector3(15,30,45) * Time.deltaTime);
+}
